8afdb49b503fafd23e9390f04b003501b3ff8911
karloVeliki <perkovickarlo5@gmail.com>
2024-03-18 19:35:29 +0100

    klasa

---
+++ NewClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pis_1Lab
+{
+    internal class NewClass
+    {
+    }
+}
